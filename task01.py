02e8b2119fa8cea7438180a57ca52f0040996896
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-11-05 23:34:38 -0800

    .

--- task01.py
+++ task01.py
@@ -64,10 +64,60 @@ Enter in the scores for 10 students for Assignment 2:
 import json
 
 assignments = []
-scores = {}
+assignment_scores = {}
 
-def assignment(name, value):
-    assignmentid= len(assignments)
-    assignments.append({"id": assignmentid, "name": name, "value": value})
-    scores[assignmentid] = {}
-    return assignmentid
+
+def create_assignment(name, value):
+    assignment_id = len(assignments)
+    assignments.append({"id": assignment_id, "name": name, "value": value})
+    assignment_scores[assignment_id] = {}
+    return assignment_id
+
+def enter_scores(assignment_id, student_scores):
+    assignment_scores[assignment_id] = {i + 1: score for i, score in enumerate(student_scores)}
+
+def get_assignment_by_id(assignment_id):
+    return next((assignment for assignment in assignments if assignment["id"] == assignment_id), None)
+
+def assignment_ui():
+    name = input("Enter assignment name: ")
+    value = float(input("Enter assignment value: "))
+    assignment_id = create_assignment(name, value)
+    print(f"Your assignment has been assigned ID {assignment_id}")
+
+def scores_ui():
+    assignment_id = int(input("Enter the assignment ID: "))
+    assignment = get_assignment_by_id(assignment_id)
+    if assignment:
+        print(f"Enter the scores for 10 students for {assignment['name']}:")
+        student_scores = [float(input(f"{i + 1}: ")) for i in range(10)]
+        enter_scores(assignment_id, student_scores)
+        print("Complete.")
+    else:
+        print("Assignment not found.")
+
+def write_to_file():
+    data = {"assignments": assignments, "assignment_scores": assignment_scores}
+    with open("assignment_data.json", "w") as file:
+        json.dump(data, file)
+
+while True:
+    print("1. Create an assignment\n2. Enter assignment scores\n3. Write your data to file")
+    choice = input("Enter in your choice: ").strip()
+    print(repr(choice))
+
+    if choice == "1":
+        assignment_ui()
+    elif choice == "2":
+        scores_ui()
+    elif choice == "3":
+        write_to_file()
+        break
+    else:
+        print("Invalid choice.")
+
+
+
+
+
+
